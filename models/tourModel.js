29f8f0043ea042160ea9c54b33aed164366256a8
koushyar <koushyar.heidari@gmail.com>
2025-03-03 17:08:53 +0330

    Connecting Our Database with the Express App + Creating a Simple Tour Model + Creating Documents and Testing the Model

---
+++ models/tourModel.js
@@ -0,0 +1,17 @@
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: String,
+    default: 4.5,
+  },
+  price: {
+    type: String,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
--- server.js
+++ server.js
@@ -1,7 +1,69 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.models.Tour.modelName);
+    // console.log(con.connections);
+    console.log('DB connection was successful');
+  });
+
+// mongoose
+//   .connect(process.env.DATABASE_LOCAL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then((con) => {
+//     // console.log(con.connections);
+//     console.log('DB is connected successfully');
+//   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: String,
+    default: 4.5,
+  },
+  price: {
+    type: String,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hikerss',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error!: ', err);
+  });
 
 // env is set to development by default by express
 // console.log(app.get('env'));
